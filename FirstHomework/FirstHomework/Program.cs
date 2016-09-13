a3f0c290e35dbbc3dfb5a6dc1c192bd8450b9f7a
Fred3267 <231194fck@gmail.com>
2016-09-12 23:44:32 -0600

    Add files via upload

---
+++ FirstHomework/FirstHomework/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sUserInput;
+            int iUserInput;
+            int iNumTeams;
+
+            Console.Write("How many teams? ");
+            iNumTeams = Console.Read();
+
+            
+        }
+    }
+    public class team
+    {
+        public string name;
+        public int wins;
+        public int loss;
+    }
+    public class soccerTeam
+    {
+        public int draw;
+        public int goalsFor;
+        public int goalsAgainst;
+        public int differential;
+        public int points;
+    }
+    public class game
+    {
+
+    }
+}
